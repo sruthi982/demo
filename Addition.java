8b7712820fb4a02b894741c57a0a940245b40f05
sruthi982 <ksruthi982@gmail.com>
2024-11-27 20:29:17 +0530

    frist work

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package basicjavaprograms;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
+
